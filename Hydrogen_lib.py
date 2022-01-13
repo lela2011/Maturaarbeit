72ee39c19ccd64b6aa077ed14b881734ffbd2229
jakob <jakob.mostert@outlook.com>
2022-01-13 11:47:01 +0100

    Added comments and docstring

--- Hydrogen_lib.py
+++ Hydrogen_lib.py
@@ -3,65 +3,107 @@ import scipy.constants as const
 import scipy.special as special
 
 
-def laguerre_polynomial_builder(r,s):
+def spherical_harmonics_builder(l: int, m: int):
+    '''
+    Creates spherical harmonic function dependent on theta and phi.
 
-    def laguerre_polynomial_callback(x):
-        L_rs = 0
-        k=0
-        while k <= (r-s):
-            L_rs += np.power(-1, k+s) * np.power(np.math.factorial(r), 2) / (np.math.factorial(k) * np.math.factorial(k+s) * np.math.factorial(r-k-s)) * np.power(x, k)
-            k += 1
-        return L_rs
-    return laguerre_polynomial_callback
+    :param l: Azimuthal quantum number
+    :type l: int
+    :param m: Magnetic quantum number
+    :type m: int
+    :return: returns a spherical harmonic for given quantum numbers
+    :rtype: functionType
+    '''
+    def spherical_harmonic(theta, phi):
+        '''
+        Spherical harmonic dependent on quantum numbers l and m.
 
-# returns spherical harmonics based on l, m, theta, phi
-# Unit: none
-# Value: complex
-def spherical_harmonics(l: int, m: int, theta: float, phi: float):
-    Y_lm = special.sph_harm(m, l, theta, phi)
-    return Y_lm
+        :param theta: azimuthal angle in spherical coordinate system, 0 ≤ theta ≤ π
+        :type theta: float
+        :param phi: polar angle in spherical coordinate system, 0 ≤ phi < 2π
+        :type phi: float
+        :return: Complex number
+        :rtype: complex
+        '''
+        return special.sph_harm(m, l, theta, phi)
+    return spherical_harmonic
 
 
-# returns a callable radial wave function dependent only on r
 def radial_wave_function_builder(z: int, n: int, l: int):
+    '''
+    Creates callable wavefunction dependent on r.
+
+    :param z: Atomic number
+    :type z: int
+    :param n: Principle quantum number
+    :type n: int
+    :param l: Azimuthal quantum number
+    :type l: int
+    :return: Radial wave function dependent on r
+    :rtype: FunctionType
+    '''
+
+    # Bohr radius
     a = const.physical_constants["Bohr radius"][0]
     k = z / (n*a)
-    laguerre_polynomial = laguerre_polynomial_builder(n+l, 2*l + 1)
-
-    def radial_wave_function_callback(r):
-        func = -1 * np.power((np.math.factorial(n-l-1)*np.power(2*k, 3)) / (2 * n * np.power(np.math.factorial(n+l), 3)), 1/2) * np.power(2*k*r, l) * np.exp(-1 * k * r) * laguerre_polynomial(2 * k * r)
-        return func
-
-    return radial_wave_function_callback
-
-
-def radial_wave_function_builder_eth(z, n, l):
-    mu = const.electron_mass * const.proton_mass / (const.electron_mass + const.proton_mass)
-    a = const.physical_constants["Bohr radius"][0]*const.electron_mass/mu
+    # generalized laguerre polynomial
     laguerre_polynomial = special.genlaguerre(n-l-1, 2*l+1)
+    # Normalisation constant A
+    norm_const = ((np.math.factorial(n - l - 1) * (2 * k) ** 3) / (2 * n * (np.math.factorial(n + l)) ** 3)) ** (1 / 2)
 
-    def fun(r):
-        val = np.sqrt((np.math.factorial(n-l-1))/(2*n*np.math.factorial(n+l))) * np.power((2*z)/(n*a), 3/2) * np.power((2*z*r)/(n*a),l) * np.exp((-z*r)/(n*a)) * laguerre_polynomial((2*z*r)/(n*a))
-        return val
+    def radial_wave_function(r):
+        '''
+        Returns the value of the radial wave function dependent on r
 
-    return fun
+        :param r: distance to origin
+        :type r: float
+        :return: value of radial wave function
+        '''
 
-# Calculates eigenvalues of coulomb-potential based on z, n, electron mass, proton mass
-# Unit: eV, electron volt
-# Value: real
-def eva_coulomb_potential_electronVolt(z: int, n: int, mass_e: float, mass_p: float):
-    # Reduzierte Masse
+        return -1 * norm_const * (2*k*r) ** l * np.e ** (-1 * k * r) * laguerre_polynomial(2 * k * r)
+
+    return radial_wave_function
+
+
+def eva_coulomb_potential_electronVolt(z, n, mass_e, mass_p):
+    '''
+    Calculates the eigenvalues for the coulomb potential of a hydrogen Atom dependent on z, n, electron mass and proton mass
+
+    :param z: Atomic number
+    :type z: int
+    :param n: Principle quantum number
+    :type n: int
+    :param mass_e: Mass of electron
+    :type mass_e: float
+    :param mass_p: Mass of proton
+    :type mass_p: float
+    :return: Energy eigenvalue of hydrogen atom for given z, n, electron mass and proton mass in electron volts
+    :rtype: float
+    '''
+
+    # reduced mass
     mu = mass_e * mass_p / (mass_e + mass_p)
     # Energieeigenwerte Coulomb-Potential in eV
-    e = - (mu * const.c ** 2) / (2 * const.e) * const.alpha ** 2 * z ** 2 / n ** 2
-    return e
+    return - (mu * const.c ** 2) / (2 * const.e) * const.alpha ** 2 * z ** 2 / n ** 2
 
-# Calculates eigenvalues of coulomb-potential based on z, n, electron mass, proton mass
-# Unit: J, joules
-# Value: real
-def eva_coulomb_potential_joule(z: int, n: int, mass_e: float, mass_p: float):
+
+def eva_coulomb_potential_joule(z, n, mass_e, mass_p):
+    '''
+    Calculates the eigenvalues for the coulomb potential of a hydrogen Atom dependent on z, n, electron mass and proton mass
+
+    :param z: Atomic number
+    :type z: int
+    :param n: Principle quantum number
+    :type n: int
+    :param mass_e: Mass of electron
+    :type mass_e: float
+    :param mass_p: Mass of proton
+    :type mass_p: float
+    :return: Energy eigenvalue of hydrogen atom for given z, n, electron mass and proton mass in joules
+    :rtype: float
+    '''
+
     # reduced mass
     mu = mass_e * mass_p / (mass_e + mass_p)
     # eigenvalues coulomb potential in joules
-    e = - mu * const.c ** 2 / 2 * const.alpha ** 2 * z ** 2 / n ** 2
-    return e
+    return - mu * const.c ** 2 / 2 * const.alpha ** 2 * z ** 2 / n ** 2
