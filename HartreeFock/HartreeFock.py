c7da57922d454f706d2d8ff59534ad3c7ce26195
Jakob Mostert <jakob.mostert@kantiwattwil.ch>
2022-08-27 18:07:14 +0200

    Added comments to HartreeFock.py Added random initial guess functionality

--- HartreeFock/HartreeFock.py
+++ HartreeFock/HartreeFock.py
@@ -12,51 +12,79 @@ from Objects.Molecule import Molecule
 
 if __name__ == "__main__":
 
-    fluor_1 = Atom("F", (0.0, 0.0, 0.0))
-    fluor_2 = Atom("F", (0.3, 0.0, 0.0))
+    # Define Atoms
+    f = Atom("F", (0.0, 0.0, 0.0))
+    h = Atom("H", (0.0, 0.0, 1.73))
 
-    fluor_molecule = Molecule([fluor_1, fluor_2])
+    # Define Molecule
+    molecule = Molecule([f, h])
 
-    pyscf_name = fluor_molecule.pyscf_molecule_string()
+    # Generate pyscf string to build pyscf molecule
+    pyscf_name = molecule.pyscf_molecule_string()
 
+    # Build pyscf molecule
     mol = gto.M(atom = pyscf_name, basis="STO-3G")
 
+    # Generate integrals used for caluclations
     s = mol.intor("int1e_ovlp", hermi=1)
     kin = mol.intor("int1e_kin", hermi=1)
     nuc = mol.intor("int1e_nuc", hermi=1)
     ERIs = mol.intor("int2e")
 
+    # Calculate transformation matrix based on overlap matrix
     transformation = Transformation(s)
+
+    # Generate Core-Hamiltonian based on kinetic integrals and nuclear attraction integrals
     core = Core(kin, nuc)
 
+    # Initial Guess
+    last_coefficient = Coefficient(mol.nao)
+
+    # Print initial guess to check if algorithm is messing up somewhere
+    print("Initial guess: {}\n----------------------------".format(last_coefficient.matrix))
+
+    # Set energy to "unreachable" value for first iteration so convergence check is not triggered
+    last_energy : float = 1000
+
+    # set variable used to check for convergence
     converges = False
 
-    last_coefficient = Coefficient(mol.nao)
-    last_energy : float = 0
-
+    # iterate until energies converge
     while not converges:
         
+        # generate Fock matrix
         fock = Fock(core, last_coefficient, ERIs)
 
+        # transform Fock matrix
         transformed_fock = np.linalg.multi_dot([transformation.matrix.transpose(), fock.matrix, transformation.matrix])
 
+        # diagonalize tranformed Fock matrix to find eigenenergy and eigenvectors
         eigen_energy, transformed_eigen_vec = np.linalg.eig(transformed_fock)
 
+        # order eigenenergies and corresponding eigenvectors
+        idx = eigen_energy.argsort()
+        eigen_energy = eigen_energy[idx]
+        transformed_eigen_vec = transformed_eigen_vec[:,idx]
+
+        # transform coefficients to untransformed state
         coefficient = np.dot(transformation.matrix, transformed_eigen_vec)
 
+        # sum energy to be used to check convergence
         energy = np.sum(eigen_energy)
 
-        energy_diff = np.abs(last_energy - energy)
+        # calculate energy difference
+        energy_diff = energy - last_energy
 
+        # store energy for next iteration
         last_energy = energy
 
+        # generate new coefficient matrix
         last_coefficient = Coefficient(mol.nao, old_matrix = coefficient)
 
-        if energy_diff <= 0.1:
+        # check for convergence
+        if 0 <= energy_diff <= 0.0001:
             converges = True
 
+    #print found energies
     print(eigen_energy)
-
-
-
 
